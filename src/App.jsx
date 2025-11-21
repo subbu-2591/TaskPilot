554d2db6c7771c3c1a5e35dee10219c209ac8fe4
subrahmanyag79@gmail.com <subrahmanyag79@gmail.com>
2025-11-21 17:22:26 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState,useEffect } from 'react';
+import Taskform from './Components/Taskform'
+import Tasklist from './Components/Tasklist'
+import Progresstracker from './Components/Progresstracker'
+
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+  });
+
+
+  const addTask = (task)=>{
+    setTasks([...tasks, task])
+  }
+
+
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+  const deleteTask = () => {
+
+
+  }
+  return (
+    <div>
+      <header>
+        <h1>TaskMan</h1>
+        <p><i>Your friendly Task Manager</i></p>
+      </header>
+      <Taskform addTask = {addTask}/>
+      <Tasklist tasks = {tasks}
+       updateTask = {updateTask} 
+       deleteTask = {deleteTask}/>
+      <Progresstracker />
+      <button>clear All Tasks</button>
+    </div>
+  )
+}
---
+++ src/Components/Tasklist.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Tasklist({tasks , updateTask, deleteTask}) {
+  const toggleComplete = (index) => {
+   const updatedTask = {...tasks[index], 
+    completed: !tasks[index].completed};
+   updateTask(updatedTask, index);
+  }
+  return (
+    <ul>
+      {tasks.map((task, index)=>(
+        <li key={index}>
+          <div>
+            <span>{task.text}</span>
+            <small>{task.priority} , 
+              {task.category}</small>
+          </div>
+          <div>
+            <button onClick={()=>{toggleComplete(index)}}>
+              {task.completed ? "Undo" : "Complete"}</button>
+            <button>delete</button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
